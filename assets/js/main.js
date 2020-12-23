fb239032fcfe7f3eb09fe11278fa19623627730f
IrishCortez <IrishCortez@users.noreply.github.com>
2020-12-23 13:00:55 +0800

    CSS and JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,16 @@
 jQuery(document).ready(function($) {
     /*======= Skillset *=======*/
     $('.level-bar-inner').css('width', '0');
+    $('.level-bar-inner2').css('width', '0');
     $(window).on('load', function() {
         $('.level-bar-inner').each(function() {
+            var itemWidth = $(this).data('level');
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+        });
+
+        $('.level-bar-inner2').each(function() {
             var itemWidth = $(this).data('level');
             $(this).animate({
                 width: itemWidth
